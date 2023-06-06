56d1447df8a9bf7806b04e43c0dd9f95266dfbfc
Kesha <cyberkesha41@gmail.com>
2023-06-06 10:38:17 +0300

    add comments

--- src/the_command_pattern/LightOnCommand.java
+++ src/the_command_pattern/LightOnCommand.java
@@ -1,11 +1,13 @@
 package the_command_pattern;
 
 public class LightOnCommand implements Command{
+	// Исполнитель команды 
 	Light light;
-	
+	// Команда завернутая в класс-объект
 	// constructor
 	public LightOnCommand(Light light) {
-		// don't shadow
+		// Параметр light затеняет член класса light, поэтому через this
+		// Тот кто будет исполнять команду
 		this.light = light;
 	}
 
