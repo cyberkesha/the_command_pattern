c3db13c2ac8966a10ceb0a08547c6a891c624d67
Kesha <cyberkesha41@gmail.com>
2023-06-06 10:48:15 +0300

    implement SimpleRemoteControl

--- src/the_command_pattern/SimpleRemoteControl.java
+++ src/the_command_pattern/SimpleRemoteControl.java
@@ -1,5 +1,18 @@
 package the_command_pattern;
 
 public class SimpleRemoteControl {
+	Command slot;
+	
+	//constructor
+  public SimpleRemoteControl() {
+	}
+  
+  public void setCommand(Command command) {
+		slot = command;
+	}
+	
+  public void buttonWasPressed() {
+		slot.execute();
+	}
 
 }
