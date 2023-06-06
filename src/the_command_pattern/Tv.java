7e5a2a23d5408465915032124b0ea7d0c4067c65
Kesha <cyberkesha41@gmail.com>
2023-06-06 14:39:23 +0300

    rename methods in TV class

--- src/the_command_pattern/Tv.java
+++ src/the_command_pattern/Tv.java
@@ -2,11 +2,11 @@ package the_command_pattern;
 
 public class Tv {
 	// Исполняющий команду
-	public void turnOnDisplay() {
-		System.out.print("Display is turned on\n");	
+	public void tvTurnOn() {
+		System.out.print("Tv is turned on\n");	
 	}
 	
-	public void turnOffDisplay() {
-		System.out.print("Display is turned off\n");
+	public void tvTurnOff() {
+		System.out.print("Tv is turned off\n");
 	}
 }
---
+++ src/the_command_pattern/TvOn.java
@@ -0,0 +1,5 @@
+package the_command_pattern;
+
+public class TvOn {
+
+}
