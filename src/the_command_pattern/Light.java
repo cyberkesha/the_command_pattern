a6558c41ecf4a1b3d76b4c4457e72c0ea0e2b8d2
Kesha <cyberkesha41@gmail.com>
2023-06-06 10:17:38 +0300

    Light on off methods

--- src/the_command_pattern/Light.java
+++ src/the_command_pattern/Light.java
@@ -2,4 +2,12 @@ package the_command_pattern;
 
 public class Light {
 
+	public void on() {
+		System.out.print("Light is ON\n");
+	}
+	
+	public void off() {
+		System.out.print("Light is OFF\n");
+	}
+	
 }
