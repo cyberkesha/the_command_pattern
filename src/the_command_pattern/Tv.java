85e8a7b07b52a5ce24cde0a0c154450f1958365c
Kesha <cyberkesha41@gmail.com>
2023-06-06 15:29:04 +0300

    edit main

--- src/the_command_pattern/Tv.java
+++ src/the_command_pattern/Tv.java
@@ -3,10 +3,10 @@ package the_command_pattern;
 public class Tv {
 	// Исполняющий команду
 	public void turnOn() {
-		System.out.print("Tv is turned on\n");	
+		System.out.print("Tv is turned ON\n");	
 	}
 	
 	public void turnOff() {
-		System.out.print("Tv is turned off\n");
+		System.out.print("Tv is turned OFF\n");
 	}
 }
