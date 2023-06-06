e82c6b405ddce1e3f3823f879c5b86f6e6ee1d85
Kesha <cyberkesha41@gmail.com>
2023-06-06 15:07:04 +0300

    editing main

--- src/the_command_pattern/Main.java
+++ src/the_command_pattern/Main.java
@@ -6,22 +6,33 @@ public class Main {
 		// Инициатор исполнения команды
 		SimpleRemoteControl remote = new SimpleRemoteControl();
 
+		System.out.print("========================\n");
+		System.out.print("Light:\n\n");
+		
 		// Объект, который будет выполнять "команду" от инициатора
 		// Получатель запроса
 		Light light = new Light();
 		
 		// Команда с получателем (исполнителем команды)
 		LightOnCommand lightOn = new LightOnCommand(light);
-		
 		LightOffCommand lightOff = new LightOffCommand(light);
 		
 		remote.setCommand(0, lightOn, lightOff);
-	
-		System.out.print("Multiply commands\n");
-		
+
 		// По нажатию выполняется команда-объект
 		remote.onButtonWasPressed(0);
 		remote.offButtonWasPressed(0);
 		
+		System.out.print("========================\n");
+		System.out.print("Tv:\n\n");
+		
+		Tv tv = new Tv();
+		TvOnCommand tvOn = new TvOnCommand(tv);
+		TvOffCommand tvOff = new TvOffCommand(tv);
+		
+		remote.setCommand(1, tvOn, tvOff);
+		
+		remote.onButtonWasPressed(1);
+		remote.offButtonWasPressed(1);
 	}
 }
