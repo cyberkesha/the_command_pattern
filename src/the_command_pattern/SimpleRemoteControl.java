1a1f45aa12414db46630c59cce2b0d0836ba37c1
Kesha <cyberkesha41@gmail.com>
2023-06-06 13:53:43 +0300

    array commands RemoteControl

--- src/the_command_pattern/SimpleRemoteControl.java
+++ src/the_command_pattern/SimpleRemoteControl.java
@@ -1,18 +1,29 @@
 package the_command_pattern;
 
 public class SimpleRemoteControl {
-	Command slot;
-	// Контроллер - исполнения - инициатор
+	// Объявили массивы объектов типа интерфейс Command
+	Command[] onCommands;
+	Command[] offCommands;
+
+	private int maxLimit = 7;
 	
 	//constructor
   public SimpleRemoteControl() {
-	}
+  	// Создать объекты массивы команд
+  	onCommands  = new Command[maxLimit];
+  	offCommands = new Command[maxLimit];
+  }
   
-  public void setCommand(Command command) {
-		slot = command;
+  public void setCommand(int slot, Command onCommand, Command offCommand) {
+		onCommands[slot]  = onCommand;
+		offCommands[slot] = offCommand;
 	}
 	
-  public void buttonWasPressed() {
-		slot.execute();
+  public void onButtonWasPressed(int slot) {
+		onCommands[slot].execute();
+	}
+  
+  public void offButtonWasPressed(int slot) {
+		offCommands[slot].execute();
 	}
 }
