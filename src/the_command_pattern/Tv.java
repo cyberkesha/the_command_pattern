4be0bbc326fd0a4a9f3fa94c54ff3e74d39fecaa
Kesha <cyberkesha41@gmail.com>
2023-06-06 14:34:12 +0300

    impl C_Tv

--- src/the_command_pattern/Tv.java
+++ src/the_command_pattern/Tv.java
@@ -2,4 +2,11 @@ package the_command_pattern;
 
 public class Tv {
 	// Исполняющий команду
+	public void turnOnDisplay() {
+		System.out.print("Display is turned on\n");	
+	}
+	
+	public void turnOffDisplay() {
+		System.out.print("Display is turned off\n");
+	}
 }
